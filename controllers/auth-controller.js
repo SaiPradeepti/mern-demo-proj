ff02ac7592ddddec0d1b163e6cee0489513264c1
Sai Pradeepti <saipradeepti14@gmail.com>
2024-04-01 02:17:08 +0530

    Storing registered user data using postman

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,7 @@
 // In an Express.js application, a controller refers to a part of your code that is responsible for handling the application's logic. Controllers are typically used to process incoming requests, interact with models (data sources), and send responses back to clients. They help organize your application by separating concerns and following the MVC (Model-View-Controller) design pattern.
 
+const User = require('../models/user-model')
+
 const home = async (req, res) => {
     try {
          res.status(200).send("Hello!!")
@@ -10,8 +12,19 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
     try {
-        console.log(req.body)
-        res.status(200).json({message: req.body})
+        const {username, email, phone, password} = req.body;
+
+        // const userExist = User.findOne({email: email})
+        const userExist = await User.findOne({email})
+
+        if(userExist){
+            return res.status(400).json({message: "Email already exists"})
+        }
+
+        const userCreated = await User.create({username, email, phone, password})
+
+        // console.log(data)
+        res.status(200).json({message: userCreated})
     } catch (error) {
         res.status(500).json("internal server error")
     }
