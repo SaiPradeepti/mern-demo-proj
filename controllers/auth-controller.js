a33bd53702e883290d4901c18bc729eec6eca752
Sai Pradeepti <saipradeepti14@gmail.com>
2024-04-02 11:52:05 +0530

    Secure User Password using Bcrypt.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,7 @@
 // In an Express.js application, a controller refers to a part of your code that is responsible for handling the application's logic. Controllers are typically used to process incoming requests, interact with models (data sources), and send responses back to clients. They help organize your application by separating concerns and following the MVC (Model-View-Controller) design pattern.
 
 const User = require('../models/user-model')
+const bcrypt = require('bcrypt')
 
 const home = async (req, res) => {
     try {
@@ -21,12 +22,17 @@ const register = async (req, res) => {
             return res.status(400).json({message: "Email already exists"})
         }
 
+        // hash the password
+        // greater the saltRound the more complex the hasing of passwrod is and at the same time more time consuming
+        // const saltRounds = 10 
+        // const hash_password = await bcrypt.hash(password, saltRounds)
+
         const userCreated = await User.create({username, email, phone, password})
 
         // console.log(data)
         res.status(200).json({message: userCreated})
     } catch (error) {
-        res.status(500).json("internal server error")
+        res.status(500).json(`internal server error: ${error}`)
     }
 }
 
--- models/user-model.js
+++ models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     username : {
@@ -21,6 +22,22 @@ const userSchema = new mongoose.Schema({
     isAdmin : {
         type: Boolean,
         default: false,
+    }
+})
+
+// secure the password with the bcrypt
+// the below code will run before the user is created in the database
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if(!user.isModified('password'))
+        next()
+
+    try {
+        const saltRounds = await bcrypt.genSalt(10)
+        const hash_password = await bcrypt.hash(user.password, saltRounds)
+        user.password = hash_password;
+    } catch (error) {
+        next(error);
     }
 })
 
